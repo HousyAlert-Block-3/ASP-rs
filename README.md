71f624cfc86b6fb2fb8ec55331a99577ea5ca214
Jacob Ellington <cannotilever@protonmail.com>
2024-03-22 09:33:39 -0400

    start on conversion from bytes

--- README.md
+++ README.md
@@ -7,3 +7,19 @@ This is the implementation of the alarm signaling protocol in Rust for the Housy
 
 (Diagram is not to scale)
 
+This protocol's byte order is big endian.
+
+The Alarm Type byte stores a series of characteristics 
+of the signaled alarm as a series of booleans where 1 indicates
+that the aspect is true about the alarm condition.
+
+| Bit | Meaning  | Notes                                    |
+|-----|----------|------------------------------------------|
+| 0   | Silent   | Only notify select staff                 |
+| 1   | Browser  | Enable browser extension alerting        |
+| 2   | Lockdown | Indicates a shelter in place instruction |
+| 3   | Evacuate |                                          |
+| 4   | Reserved | Reserved for future use                  |
+| 5   | Reserved | Reserved for future                      |
+| 6   | Intruder |                                          |
+| 7   | Fire     |                                          |
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::io::{Error, Read};
 use rsa::RsaPrivateKey;
 use rsa::pkcs1v15::{Signature, SigningKey, VerifyingKey};
 use rsa::signature::{Keypair, RandomizedSigner, SignatureEncoding, Verifier};
@@ -22,18 +23,63 @@ impl ASP {
         Ok(())
     }
     pub fn broadcast(&self, msg: ASPMessage) -> Result<(), io::Error> {
-        
-        Ok(())
+        todo!()
     }
     pub fn try_receive(&self) -> Result<ASPMessage, io::Error> {
-        
+        todo!()
     }
 }
 
 pub struct ASPMessage {
     activator_name: String,
-    alarm_code: u8, //Raw byte of data
-    signature: Signature
+    alarm_details: Vec<AlarmDetail>,
+    alarm_type: AlarmType,
+    signature: Signature,
+    raw: [u8; 37],
+}
+impl TryFrom<&[u8]> for ASPMessage {
+    fn try_from(value: &[u8]) -> Result<Self, Error> {
+        let message_vec = value.to_vec();
+        if message_vec.len() != 164 {
+            return Err(data_err("Incorrect message length"));
+        }
+        let namevec: Vec<u8> = message_vec[0..32].to_vec();
+        let alarm_code = message_vec[32];
+        let timebytes: [u8; 4] = message_vec[33 .. 38].try_into()
+            .map_err(|_err|data_err("Invalid Timestamp"))?;
+
+        Ok(ASPMessage {
+            activator_name: String::from_utf8(namevec)
+                .map_err(|_err|Error::new(io::ErrorKind::InvalidData,"Bad name data"))?,
+            alarm_code: ,
+            signature: Signature::try_from(sigarray)?
+        })
+    }
+    type Error = Error;
+}
+
+impl ASPMessage{
+    fn verify_sig(&self, pubkey: VerifyingKey<Sha256>) -> Result<(), io::Error> {
+        pubkey.verify(&self.raw, &self.signature)
+            .map_err(|err|data_err(format!("Signature Invalid! {}", err.to_string()).as_str()))?;
+        Ok(())
+    }
+}
+
+fn data_err(msg: &str) -> io::Error {
+    Error::new(io::ErrorKind::InvalidData, msg)
+}
+
+enum AlarmDetail {
+    Silent,
+    Browser,
+    Lockdown,
+    Evacuate,
+}
+
+enum AlarmType {
+    Intruder,
+    Fire
 }
 
 pub fn example1() -> rsa::signature::Result<()> {
